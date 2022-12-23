3e79749755edcb35032c4d1be0696abadbef564a
Salt <nicolft@gmail.com>
2022-12-23 00:20:15 -0500

    Add Actor and related object skeletons.

--- arc-mutex/src/lib.rs
+++ arc-mutex/src/lib.rs
@@ -1,3 +1,28 @@
+use std::collections::HashSet;
+use std::sync::{Arc, Mutex};
+use tokio::sync::mpsc;
+
+type ActorSet<T> = Arc<Mutex<HashSet<T>>>;
+struct Actor<T> {
+    rx: mpsc::Receiver<Command<T>>,
+    map: ActorSet<T>,
+}
+
+impl <T> Actor<T> {
+    fn new(set: ActorSet<T>, rx: mpsc::Receiver<T>) -> Self {
+        todo!()
+    }
+
+    async fn run() -> Result<(),()> {
+        todo!()
+    }
+}
+
+enum Command<T> {
+    Insert(T),
+    Delete(T),
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
